4f586599a941cb8866d24e105cfe3d744db0a293
tailan <tailansanttos02@gmail.com>
2025-08-19 19:17:57 -0300

    feat: repositorio de Usuario criado.

--- sistema-de-restaurante/src/main/java/com/tailan/sistema_de_restaurante/repositories/UserRepository.java
+++ sistema-de-restaurante/src/main/java/com/tailan/sistema_de_restaurante/repositories/UserRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<UUID, User> {
+public interface UserRepository extends JpaRepository<User,UUID> {
    Optional<User> findByEmail(String email);
 }
