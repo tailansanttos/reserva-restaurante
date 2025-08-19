c2d95836ec68c21797694073347273e51fe30171
tailan <tailansanttos02@gmail.com>
2025-08-19 17:48:01 -0300

    feat: repositorio de Usuario criado.

--- sistema-de-restaurante/src/main/java/com/tailan/sistema_de_restaurante/repositories/UserRepository.java
+++ sistema-de-restaurante/src/main/java/com/tailan/sistema_de_restaurante/repositories/UserRepository.java
@@ -2,10 +2,13 @@ package com.tailan.sistema_de_restaurante.repositories;
 
 import com.tailan.sistema_de_restaurante.model.usuario.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UUID, User> {
+   Optional<User> findByEmail(String email);
 }
