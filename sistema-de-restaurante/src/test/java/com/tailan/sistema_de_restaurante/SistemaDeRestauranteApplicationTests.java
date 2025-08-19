089e64d9470f67993a851b9ccfd615edde86d5cd
tailan <tailansanttos02@gmail.com>
2025-08-19 16:34:44 -0300

    feat: first commit

---
+++ sistema-de-restaurante/src/test/java/com/tailan/sistema_de_restaurante/SistemaDeRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tailan.sistema_de_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
