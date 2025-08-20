5e471a9246265e0f32a8ca648f31560cbcdd2fa7
tailan <tailansanttos02@gmail.com>
2025-08-20 14:19:40 -0300

    feat: controller de reservation criado e implementado.

--- sistema-de-restaurante/src/main/java/com/tailan/sistema_de_restaurante/controllers/ReservationController.java
+++ sistema-de-restaurante/src/main/java/com/tailan/sistema_de_restaurante/controllers/ReservationController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,9 +22,23 @@ public class ReservationController {
     }
 
     @PostMapping("/{mesaId}")
-    public ResponseEntity<ApiResponseDto> reservarMesa(@PathVariable("mesaId")UUID mesaID, @RequestBody @Valid ReservaRequestDto reservaRequestDto){
-        ReservaResponseDto reservaResponse = reservaService.reservarMesa(mesaID, reservaRequestDto);
+    public ResponseEntity<ApiResponseDto> reservarMesa(@PathVariable("mesaId")UUID mesaId, @RequestBody @Valid ReservaRequestDto reservaRequestDto){
+        ReservaResponseDto reservaResponse = reservaService.reservarMesa(mesaId, reservaRequestDto);
         ApiResponseDto apiResponse = new ApiResponseDto(reservaResponse, "Reserva realizada com sucesso!");
         return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponseDto> listarReservas(){
+        List<ReservaResponseDto> listReservaResponse = reservaService.listReservationsResponse();
+        ApiResponseDto apiResponse = new ApiResponseDto(listReservaResponse, "Reserva realizada com sucesso!");
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/{reservaId}")
+    public ResponseEntity<ApiResponseDto> cancelarReserva(@PathVariable("reservaId")UUID reservaId){
+       reservaService.cancelReserva(reservaId);
+        ApiResponseDto apiResponse = new ApiResponseDto(reservaId, "Reserva realizada com sucesso!");
+        return new ResponseEntity<>(apiResponse, HttpStatus.NO_CONTENT);
     }
 }
