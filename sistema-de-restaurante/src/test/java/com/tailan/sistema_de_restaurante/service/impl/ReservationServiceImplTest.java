79f68f3a824a1532615d9c663a2c3023b5d957a7
tailan <tailansanttos02@gmail.com>
2025-08-21 01:16:24 -0300

    feat: implementado Test que dever ser sucessido ao criar uma reserva.

---
+++ sistema-de-restaurante/src/test/java/com/tailan/sistema_de_restaurante/service/impl/ReservationServiceImplTest.java
@@ -0,0 +1,85 @@
+package com.tailan.sistema_de_restaurante.service.impl;
+
+import com.tailan.sistema_de_restaurante.dto.reserva.ReservaRequestDto;
+import com.tailan.sistema_de_restaurante.dto.reserva.ReservaResponseDto;
+import com.tailan.sistema_de_restaurante.mapper.ReservaMapper;
+import com.tailan.sistema_de_restaurante.model.mesa.Mesa;
+import com.tailan.sistema_de_restaurante.model.reserva.Reservation;
+import com.tailan.sistema_de_restaurante.model.reserva.StatusReserva;
+import com.tailan.sistema_de_restaurante.model.usuario.User;
+import com.tailan.sistema_de_restaurante.repositories.ReservationRepository;
+import com.tailan.sistema_de_restaurante.service.MesaService;
+import com.tailan.sistema_de_restaurante.util.AuthUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ReservationServiceImplTest {
+    @Mock
+    private ReservationRepository reservationRepository;
+    @Mock
+    private MesaService mesaService;
+    @Mock
+    private AuthUtil authUtil;
+    @Mock
+    private ReservaMapper reservaMapper;
+
+    @InjectMocks
+    private ReservationServiceImpl reservationServiceImpl;
+
+    private UUID mesaId;
+    private User usuario;
+    private Mesa mesa;
+    private ReservaRequestDto reservaRequestDto;
+
+    @BeforeEach
+    public void setUp(){
+        mesaId = UUID.randomUUID();
+        usuario = new User();
+        usuario.setId(UUID.randomUUID());
+        usuario.setEmail("teste@email.com");
+
+        mesa = new Mesa();
+        mesa.setId(mesaId);
+
+        reservaRequestDto = new ReservaRequestDto(LocalDateTime.now().plusHours(4), 5);
+    }
+    @Test
+    void deveReservarMesaComSucesso() {
+        when(authUtil.getUsuarioLogado()).thenReturn(usuario);
+        when(mesaService.getMesa(any(UUID.class))).thenReturn(mesa);
+
+        Reservation reservaSalva = new Reservation();
+        reservaSalva.setMesa(mesa);
+        reservaSalva.setUser(usuario);
+        reservaSalva.setReservationDate(reservaRequestDto.reservationDate());
+        reservaSalva.setStatus(StatusReserva.ATIVO);
+        when(reservationRepository.save(any(Reservation.class))).thenReturn(reservaSalva);
+
+
+        ReservaResponseDto responseReserva = new ReservaResponseDto(null,null,null, null, UUID.randomUUID());
+        when(reservaMapper.entityToDtoResponse(any(Reservation.class))).thenReturn(responseReserva);
+
+        ReservaResponseDto resultado = reservationServiceImpl.reservarMesa(mesaId, reservaRequestDto);
+
+        assertNotNull(resultado);
+
+        verify(mesaService).validarMesaReserva(mesaId, reservaRequestDto.quantidadePessoas());
+        verify(reservationRepository).save(any(Reservation.class));
+
+        verify(mesaService).atualizarStatusMesaReservada(mesaId);
+
+    }
+}
